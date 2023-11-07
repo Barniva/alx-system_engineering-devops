52935f02cafc52b0b108d694a16754b2596ecad3
Barnabas Endashaw <123116734+Barniva@users.noreply.github.com>
2023-11-08 01:47:40 +0300

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""This python module contains a script that interacts with the Reddit API"""
-from requests import get
+"""Contains recurse function"""
+import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Queries the reddit api and returns a list of containing the titles
-    of all hot articles for a given reddit."""
-    header = {"User-Agent": "alx_project"}
-    params = {"after": after, "count": count}
-    res = get("https://www.reddit.com/r/{}/hot/.json".format(subreddit),
-              headers=header, allow_redirects=False, params=params)
-    if res.status_code == 404:
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
-    after = res.json().get("data").get("after")
-    count += res.json().get("data").get("dist")
-    for r in res.json().get("data").get("children"):
-        hot_list.append(r.get("data").get("title"))
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
     return hot_list
